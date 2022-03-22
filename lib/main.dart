780a6b0231936b24b73aeb0a7a8967dec2e65b2f
Ramyasri0711 <ramyasri0711@gmail.com>
2022-03-22 15:45:38 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/person.png'),
+              ),
+              Text(
+                'Ramyasri R',
+                style: TextStyle(
+                  fontFamily: 'DancingScript',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro-Regular',
+                  fontSize: 18.0,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                child: Divider(
+                  color: Colors.white,
+                  thickness: 3.0,
+                ),
+              ),
+              Card(
+                //padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal[900],
+                      ),
+                      SizedBox(
+                        width: 45.0,
+                      ),
+                      Text(
+                        '+91 98745 63210',
+                        style: TextStyle(
+                          color: Colors.teal[900],
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.all(10.0),
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal[900],
+                      ),
+                      SizedBox(
+                        width: 25.0,
+                      ),
+                      Text(
+                        'ramyasri0711@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal[900],
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
